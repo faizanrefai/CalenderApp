3769b4814340ae39dd3f2f22cf0303e6cfd0a7cb
Ronak Arora <ronak@itechcoders.com>
2013-02-18 14:18:02 +0530

    Initial Commit

---
+++ CalenderApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalenderApp
+//
+//  Created by i Tech Coders Pvt Ltd. on 18/02/13.
+//  Copyright (c) 2013 i Tech Coders Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CalenderApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CalenderApp
+//
+//  Created by i Tech Coders Pvt Ltd. on 18/02/13.
+//  Copyright (c) 2013 i Tech Coders Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
